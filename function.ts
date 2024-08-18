95a7311bc6bdc803ce6799be9d3dfab57e0d24c5
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-18 21:32:34 +0600

    arrow func with default value

--- function.ts
+++ function.ts
@@ -1,26 +1,34 @@
-function addTow(num:number) {
-    return num + 2
+function addTow(num: number) {
+  return num + 2;
 }
 
-addTow(4)
+addTow(4);
 
 function getUpper(value: string) {
-    return value.toUpperCase()
+  return value.toUpperCase();
 }
 
-getUpper('Afnan Hussain')
+getUpper("Afnan Hussain");
 
 // multiple value
 
-function singUp(name: string, email: string, isMale:boolean) {
-    return {
-     name,
-        email,
-          isMale
-
-    }
+function singUp(name: string, email: string, isMale: boolean) {
+  return {
+    name,
+    email,
+    isMale,
+  };
 }
 
-singUp('Afnan', 'afnan@gmail.com', true)
 
-export{}
+singUp("Afnan", "afnan@gmail.com", true);
+
+// arrow func with default value
+
+let loginUser = (name: string, email:string, isMale: boolean = true) =>{
+
+}
+
+loginUser("Afnan", "afnan@gmail.com")
+
+export {};
