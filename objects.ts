6886e90eab212f91b4bb0e4d0f062b42950cd969
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 15:51:39 +0600

    using type keyword

--- objects.ts
+++ objects.ts
@@ -3,5 +3,16 @@ function createCourse():{name:string,price: number}{
     return {name:"Next js", price: 399}
 }
 
+// using type keyword
+
+type User ={
+    name: string,
+    email: string,
+    isActive: boolean
+}
+function createUser(user: User){}
+
+createUser({name:"Afnan", email:"", isActive: true})
+
 export{}
 
