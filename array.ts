c5b3062d0318658dd56f7f06d2353bdad7c66585
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 21:29:10 +0600

    add mix data type on an array

--- array.ts
+++ array.ts
@@ -4,3 +4,9 @@ const data: number[] = [1,4,5]
 // only add string in an array
 
 const data2: string [] = ["Apple", "Banana", "Orange"]
+
+// add mix data type on an array
+
+const data3:(string | number | boolean)[] =[1, "Apple", true]
+
+export{}
