c5e4d3eef68f34202c408dc9bd1efa839693ff60
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 21:17:55 +0600

    can change the type of data

--- union.ts
+++ union.ts
@@ -4,3 +4,21 @@ let score: number | string | boolean = 33
 
 score = "My Score is 89"
 score = false
+
+// can change the type of data
+
+type User = {
+    name:string,
+    id: number
+}
+type Admin = {
+    username:string,
+    id: number
+}
+
+let afnan: User | Admin = {name:"Afnan", id:31}
+
+afnan = {username:"afnan", id:4}
+
+export{}
+
