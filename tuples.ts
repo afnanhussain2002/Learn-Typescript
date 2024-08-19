3049c970fb47e6c36dd8da99cba58ace602811b1
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 22:02:13 +0600

    add tuples into "type"

--- tuples.ts
+++ tuples.ts
@@ -2,5 +2,12 @@
 
 let user : [string, number, boolean]
 
-
 user = ["ah", 123, false]
+
+// add tuples into "type"
+
+type User = [number, string]
+
+const newUser: User = [122, "Afnan Hussain"]
+
+export{}
