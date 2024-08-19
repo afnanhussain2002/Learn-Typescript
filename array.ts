3e8c22d9bdc6e2ec14dfb120aa52fc143b8e4128
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 21:57:51 +0600

    make a file for learn tuples in ts

--- array.ts
+++ array.ts
@@ -6,7 +6,7 @@ const data: number[] = [1,4,5]
 const data2: string [] = ["Apple", "Banana", "Orange"]
 
 // add mix data type on an array
-
 const data3:(string | number | boolean)[] =[1, "Apple", true]
+
 
 export{}
