2d60d085a64ec6d3cbf764e343dc9efe79ae3065
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-19 18:17:04 +0600

    know About readonly and ? keyword

--- objects.ts
+++ objects.ts
@@ -14,5 +14,19 @@ function createUser(user: User){}
 
 createUser({name:"Afnan", email:"", isActive: true})
 
+
+type Super = {
+   readonly _id: string,
+   SuperName: string,
+   Pay?: number
+}
+
+let myUser: Super = {
+    _id:"1234",
+    SuperName:"A"
+}
+
+myUser.SuperName = "Afnan"
+// myUser._id = "fjjf" cant change the _id becouse it was readOnly
 export{}
 
