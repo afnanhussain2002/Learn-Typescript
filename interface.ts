68446ee1f3ecb714b8cdaeb0db389ba83a0bbffd
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-21 20:59:47 +0600

    write 1st func system in interface

--- interface.ts
+++ interface.ts
@@ -1,9 +1,15 @@
 interface User {
    email: string,
-  readonly userId: number
+  readonly userId: number,
+  goggleId?: number,
+  system:() => string
 
 }
 
-const afnan: User = {email: "afnan@gmail.com", userId:12}
+const afnan: User = {email: "afnan@gmail.com", userId:12,
+    system: () =>{
+        return "Afnan Hussain"
+    }
+}
 
 export{}
