89df3230f02a4afcb3d2da105e8c7fe6a8d7d1d0
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-21 21:01:10 +0600

    write 2nd method for interface func

--- interface.ts
+++ interface.ts
@@ -2,13 +2,17 @@ interface User {
    email: string,
   readonly userId: number,
   goggleId?: number,
-  system:() => string
+  system:() => string,
+  system2(): number
 
 }
 
 const afnan: User = {email: "afnan@gmail.com", userId:12,
     system: () =>{
         return "Afnan Hussain"
+    },
+    system2: () =>{
+        return 12
     }
 }
 
