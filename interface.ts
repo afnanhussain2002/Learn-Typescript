e5424741e41e47866049dbb7512a18ebbc19072d
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-21 21:09:15 +0600

    learn extends method with interface

--- interface.ts
+++ interface.ts
@@ -11,14 +11,19 @@ interface User{
     githubId: string
 }
 
-const afnan: User = {email: "afnan@gmail.com", userId:12,
+interface Admin extends User{
+    role:"admin" | "nadmin"
+}
+
+const afnan: Admin = {email: "afnan@gmail.com", userId:12,
     system: () =>{
         return "Afnan Hussain"
     },
     system2: (couponName:"Talu2") =>{
         return 12
     },
-    githubId:"afnanhussain2002"
+    githubId:"afnanhussain2002",
+    role:"admin"
 }
 
 export{}
