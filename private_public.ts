c06b2043264a6ae54d152a32f8ba75a1b139e067
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-22 15:59:36 +0600

    write a second syntax

--- private_public.ts
+++ private_public.ts
@@ -1,10 +1,30 @@
-class User {
-    email: string
-    name: string
-    city: string = "Sylhet"
+/* class User {
+  private  email: string
+  public  name: string
+   readonly city: string = "Sylhet"
 
-    constructor(email:string, name: string){
+
+    constructor(email:string, name: string,){
         this.email = email
         this.name = name
+   
     }
-}
+} */
+
+    // some people write it like
+
+    class User{
+        readonly city: string = "Sylhet"
+        constructor(
+            public email: string,
+            public name: string,
+           private password: string
+        ){
+
+        }
+    }
+
+const afnan = new User("afnan@gamil.com", "Afnan", "password")
+
+
+afnan.name = "Hussain"
