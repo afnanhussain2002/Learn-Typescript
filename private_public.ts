635ae324b9e235e5be40a37b05d600ef439c012e
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-22 15:49:34 +0600

    write a syntax with class

---
+++ private_public.ts
@@ -0,0 +1,10 @@
+class User {
+    email: string
+    name: string
+    city: string = "Sylhet"
+
+    constructor(email:string, name: string){
+        this.email = email
+        this.name = name
+    }
+}
